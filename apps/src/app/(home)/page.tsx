6df36864d99c1d1c1fb53bc2f53a4a04f078e1f6
Mee Reak <138286828+MeeReak@users.noreply.github.com>
2024-06-01 10:51:18 +0700

    feat(auth): change authentication method
    BREAKING CHANGE: The authentication method has been changed to OAuth2. Users need to re-authenticate.

--- apps/src/app/(home)/page.tsx
+++ apps/src/app/(home)/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 const Homepage = () => {
   return (
     <>
-      <div className="max-w-[1024px] m-auto space-y-5 z-10 mt-[100px] mb-20">
+      {/* <div className="max-w-[1024px] m-auto space-y-5 z-10 mt-[100px] mb-20">
         <Trending
           className="flex gap-4 max-[640px]:overflow-hidden max-[640px]:overflow-x-auto"
           topEvent={
@@ -28,7 +28,7 @@ const Homepage = () => {
               className="rounded-xl h-[250px] w-[500px] object-cover"
             />
           }
-        />
+        /> */}
 
         <FilteredCardDisplay />
       </div>
